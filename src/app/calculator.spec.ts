1364c410c186f9bf15ece53fc45fdb40acea6f48
Nicolas Molina Monroy <hi@nicobytes.com>
2022-02-10 18:08:51 -0400

    :construction: Work in matchers

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -9,4 +9,38 @@ describe('Test for Calculator', () => {
     //Assert
     expect(rta).toEqual(9);
   });
+
+  it('#multiply should return a four', () => {
+    //Arrange
+    const calculator = new Calculator();
+    //Act
+    const rta = calculator.multiply(1,4);
+    //Assert
+    expect(rta).toEqual(4);
+  });
+
+  it('#divide should return a some numbers', () => {
+    //Arrange
+    const calculator = new Calculator();
+    //Act
+    expect(calculator.divide(6,3)).toEqual(2);
+    expect(calculator.divide(5,2)).toEqual(2.5);
+  });
+
+  it('tests matchers', () => {
+    let name = 'nicolas';
+    let name2;
+
+    expect(name).toBeDefined();
+    expect(name2).toBeUndefined();
+
+    expect(1 + 3 === 4).toBeTruthy(); // 4
+    expect(1 + 1 === 3).toBeFalsy();
+
+    expect(5).toBeLessThan(10);
+    expect(20).toBeGreaterThan(10);
+
+    expect('123456').toMatch(/123/);
+    expect(['apples', 'oranges', 'pears']).toContain('oranges');
+  });
 });
