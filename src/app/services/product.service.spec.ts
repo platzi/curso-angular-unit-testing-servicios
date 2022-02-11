2389fe175541136abbe9b2d0ac5df4b46a8d915e
Nicolas Molina Monroy <hi@nicobytes.com>
2022-02-11 19:22:52 -0400

    :construction: work

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -75,7 +75,15 @@ fdescribe('ProductsService', () => {
         {
           ...generateOneProduct(),
           price: 200, // 200 * .19 = 38
-        }
+        },
+        {
+          ...generateOneProduct(),
+          price: 0, // 0 * .19 = 0
+        },
+        {
+          ...generateOneProduct(),
+          price: -100, // = 0
+        },
       ];
       //Act
       productService.getAll()
@@ -84,6 +92,8 @@ fdescribe('ProductsService', () => {
         expect(data.length).toEqual(mockData.length);
         expect(data[0].taxes).toEqual(19);
         expect(data[1].taxes).toEqual(38);
+        expect(data[2].taxes).toEqual(0);
+        expect(data[3].taxes).toEqual(0);
         doneFn();
       });
 
@@ -91,6 +101,29 @@ fdescribe('ProductsService', () => {
       const url = `${environment.API_URL}/api/v1/products`;
       const req = httpController.expectOne(url);
       req.flush(mockData);
+      httpController.verify();
+    });
+
+    it('should send query params with limit 10 and offset 3', (doneFn) => {
+      //Arrange
+      const mockData: Product[] = generateManyProducts(3);
+      const limit = 10;
+      const offset = 3;
+      //Act
+      productService.getAll(limit, offset)
+      .subscribe((data)=> {
+        //Assert
+        expect(data.length).toEqual(mockData.length);
+        doneFn();
+      });
+
+      // http config
+      const url = `${environment.API_URL}/api/v1/products?limit=${limit}&offset=${offset}`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      const params = req.request.params;
+      expect(params.get('limit')).toEqual(`${limit}`);
+      expect(params.get('offset')).toEqual(`${offset}`);
       httpController.verify();
     });
   });
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -42,7 +42,7 @@ export class ProductsService {
       map(products => products.map(item => {
         return {
           ...item,
-          taxes: .19 * item.price
+          taxes: item.price > 0 ? .19 * item.price : 0
         }
       }))
     );
